b7079b8ce0349fd423d2d0393973d484b979cf15
Noah Oliver Giron Rigonan <oliverrigonan@gmail.com>
2017-12-12 16:28:09 +0800

    API for Supplier and Customer (POS Integration)

---
+++ easyfis/POSIntegrationApiControllers/ApiPOSIntegrationMstCustomerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace easyfis.POSIntegrationApiControllers
+{
+    public class ApiPOSIntegrationMstCustomerController : ApiController
+    {
+        // ============
+        // Data Context
+        // ============
+        private Data.easyfisdbDataContext db = new Data.easyfisdbDataContext();
+
+        // ==============================
+        // GET Customer - POS Integration
+        // ==============================
+        [HttpGet, Route("api/get/POSIntegration/customer/{updateDateTime}")]
+        public List<POSIntegrationEntities.POSIntegrationMstCustomer> GetCustomerPOSIntegration(String updateDateTime)
+        {
+            var customers = from d in db.MstArticles
+                            where d.ArticleTypeId == 2
+                            && d.IsLocked == true
+                            && d.UpdatedDateTime.Date == Convert.ToDateTime(updateDateTime)
+                            select new POSIntegrationEntities.POSIntegrationMstCustomer
+                            {
+                                ManualArticleCode = d.ManualArticleCode,
+                                Article = d.Article,
+                                Address = d.Address,
+                                ContactPerson = d.ContactPerson,
+                                ContactNumber = d.ContactNumber,
+                                Term = d.MstTerm.Term,
+                                TaxNumber = d.TaxNumber
+                            };
+
+            return customers.ToList();
+        }
+    }
+}
---
+++ easyfis/POSIntegrationApiControllers/ApiPOSIntegrationMstSupplierController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace easyfis.POSIntegrationApiControllers
+{
+    public class ApiPOSIntegrationMstSupplierController : ApiController
+    {
+        // ============
+        // Data Context
+        // ============
+        private Data.easyfisdbDataContext db = new Data.easyfisdbDataContext();
+
+        // ==============================
+        // GET Supplier - POS Integration
+        // ==============================
+        [HttpGet, Route("api/get/POSIntegration/supplier/{updateDateTime}")]
+        public List<POSIntegrationEntities.POSIntegrationMstSupplier> GetSupplierPOSIntegration(String updateDateTime)
+        {
+            var suppliers = from d in db.MstArticles
+                            where d.ArticleTypeId == 3
+                            && d.IsLocked == true
+                            && d.UpdatedDateTime.Date == Convert.ToDateTime(updateDateTime)
+                            select new POSIntegrationEntities.POSIntegrationMstSupplier
+                            {
+                                Article = d.Article,
+                                Address = d.Address,
+                                ContactNumber = d.ContactNumber,
+                                Term = d.MstTerm.Term,
+                                TaxNumber = d.TaxNumber
+                            };
+
+            return suppliers.ToList();
+        }
+    }
+}
