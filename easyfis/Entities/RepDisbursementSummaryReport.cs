40f8d548598d3950287b854d08a0ff2d5c936ad3
Noah Oliver Giron Rigonan <oliverrigonan@gmail.com>
2018-01-11 13:54:59 +0800

    Disbursement Summary Report Modification

---
+++ easyfis/Entities/RepDisbursementSummaryReport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace easyfis.Entities
+{
+    public class RepDisbursementSummaryReport
+    {
+        public Int32 CVId { get; set; }
+        public String Branch { get; set; }
+        public String CVNumber { get; set; }
+        public String CVDate { get; set; }
+        public String Payee { get; set; }
+        public String Particulars { get; set; }
+        public String Bank { get; set; }
+        public String CheckNumber { get; set; }
+        public String CheckDate { get; set; }
+        public Decimal Amount { get; set; }
+    }
+}
--- easyfis/ModifiedApiControllers/ApiRepDisbursementSummaryReportController.cs
+++ easyfis/ModifiedApiControllers/ApiRepDisbursementSummaryReportController.cs
@@ -19,7 +19,7 @@ namespace easyfis.ApiControllers
         // Disbursement Summary Report List
         // ================================
         [Authorize, HttpGet, Route("api/disbursementSummaryReport/list/{startDate}/{endDate}/{companyId}/{branchId}")]
-        public List<Models.TrnDisbursement> ListDisbursementSummaryReport(String startDate, String endDate, String companyId, String branchId)
+        public List<Entities.RepDisbursementSummaryReport> ListDisbursementSummaryReport(String startDate, String endDate, String companyId, String branchId)
         {
             var disbursements = from d in db.TrnDisbursements
                                 where d.CVDate >= Convert.ToDateTime(startDate)
@@ -27,21 +27,54 @@ namespace easyfis.ApiControllers
                                 && d.MstBranch.CompanyId == Convert.ToInt32(companyId)
                                 && d.BranchId == Convert.ToInt32(branchId)
                                 && d.IsLocked == true
-                                select new Models.TrnDisbursement
+                                select new Entities.RepDisbursementSummaryReport
                                 {
-                                    Id = d.Id,
+                                    CVId = d.Id,
                                     Branch = d.MstBranch.Branch,
                                     CVNumber = d.CVNumber,
                                     CVDate = d.CVDate.ToShortDateString(),
-                                    Supplier = d.MstArticle.Article,
+                                    Payee = d.Payee,
                                     Particulars = d.Particulars,
-                                    BankId = d.BankId,
                                     Bank = d.MstArticle1.Article,
                                     CheckNumber = d.CheckNumber,
+                                    CheckDate = d.CheckDate.ToShortDateString(),
                                     Amount = d.Amount
                                 };
 
             return disbursements.ToList();
+        }
+
+        // ================================
+        // Dropdown List - Company (Filter)
+        // ================================
+        [Authorize, HttpGet, Route("api/disbursementSummaryReport/dropdown/list/company")]
+        public List<Entities.MstCompany> DropdownListDisbursementSummaryReportListCompany()
+        {
+            var companies = from d in db.MstCompanies.OrderBy(d => d.Company)
+                            select new Entities.MstCompany
+                            {
+                                Id = d.Id,
+                                Company = d.Company
+                            };
+
+            return companies.ToList();
+        }
+
+        // ===============================
+        // Dropdown List - Branch (Filter)
+        // ===============================
+        [Authorize, HttpGet, Route("api/disbursementSummaryReport/dropdown/list/branch/{companyId}")]
+        public List<Entities.MstBranch> DropdownListDisbursementSummaryReportBranch(String companyId)
+        {
+            var branches = from d in db.MstBranches.OrderBy(d => d.Branch)
+                           where d.CompanyId == Convert.ToInt32(companyId)
+                           select new Entities.MstBranch
+                           {
+                               Id = d.Id,
+                               Branch = d.Branch
+                           };
+
+            return branches.ToList();
         }
     }
 }
