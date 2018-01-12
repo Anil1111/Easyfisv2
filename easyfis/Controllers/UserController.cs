1d8b62691761ec530c3196e80f8b3b25946261f8
Noah Oliver Giron Rigonan <oliverrigonan@gmail.com>
2018-01-12 17:07:29 +0800

    User Controller Modificaiton - To Be Continue

--- easyfis/Controllers/UserController.cs
+++ easyfis/Controllers/UserController.cs
@@ -15,36 +15,82 @@ namespace easyfis.Controllers
 
                 if (!string.IsNullOrEmpty(username))
                 {
+                    // ======================
+                    // Current Logged-In User
+                    // ======================
                     var user = context.Users.SingleOrDefault(u => u.UserName == username);
-                    string fullName = string.Concat(new string[] { user.FullName });
-                    string userId = string.Concat(new string[] { user.Id });
-                    string email = string.Concat(new string[] { user.Email });
-                    string userName = string.Concat(new string[] { user.UserName });
 
-                    Data.easyfisdbDataContext db = new Data.easyfisdbDataContext();
-                    string branchId = string.Concat((from d in db.MstUsers where d.UserId == userId select d.BranchId).SingleOrDefault());
-                    string branch = string.Concat((from d in db.MstUsers where d.UserId == userId select d.MstBranch.Branch).SingleOrDefault());
-                    string company = string.Concat((from d in db.MstUsers where d.UserId == userId select d.MstBranch.MstCompany.Company).SingleOrDefault());
-                    string mstUserId = string.Concat((from d in db.MstUsers where d.UserId == userId select d.Id).SingleOrDefault());
-                    string officialReceiptName = string.Concat((from d in db.MstUsers where d.UserId == userId select d.OfficialReceiptName).SingleOrDefault());
-                    string inventoryType = string.Concat((from d in db.MstUsers where d.UserId == userId select d.InventoryType).SingleOrDefault());
-                    string defaultSalesInvoiceDiscountId = string.Concat((from d in db.MstUsers where d.UserId == userId select d.DefaultSalesInvoiceDiscountId).SingleOrDefault());
-                    string defaultSalesInvoiceDiscount = string.Concat((from d in db.MstUsers where d.UserId == userId select d.MstDiscount.Discount).SingleOrDefault());
-                    string salesInvoiceName = string.Concat((from d in db.MstUsers where d.UserId == userId select d.SalesInvoiceName).SingleOrDefault());
+                    // ==========
+                    // AspNetUser
+                    // ==========
+                    string aspNetUserId = user.Id;
+                    string userName = user.UserName;
+                    string fullName = user.FullName;
 
-                    ViewData.Add("UserId", userId);
-                    ViewData.Add("FullName", fullName);
-                    ViewData.Add("Email", email);
+                    ViewData.Add("UserId", aspNetUserId);
                     ViewData.Add("UserName", userName);
-                    ViewData.Add("BranchId", branchId);
-                    ViewData.Add("Branch", branch);
-                    ViewData.Add("Company", company);
-                    ViewData.Add("MstUserId", mstUserId);
-                    ViewData.Add("OfficialReceiptName", officialReceiptName);
-                    ViewData.Add("InventoryType", inventoryType);
-                    ViewData.Add("defaultSalesInvoiceDiscountId", defaultSalesInvoiceDiscountId);
-                    ViewData.Add("DefaultSalesInvoiceDiscount", defaultSalesInvoiceDiscount);
-                    ViewData.Add("SalesInvoiceName", salesInvoiceName);
+                    ViewData.Add("FullName", fullName);
+
+                    // =======
+                    // MstUser
+                    // =======
+                    Data.easyfisdbDataContext db = new Data.easyfisdbDataContext();
+
+                    var currentUser = from d in db.MstUsers
+                                      where d.UserId == aspNetUserId
+                                      select d;
+
+                    if (currentUser.Any())
+                    {
+                        // ==========================
+                        // Current Branch and Company
+                        // ==========================
+                        int branchId = currentUser.FirstOrDefault().BranchId;
+                        string branch = currentUser.FirstOrDefault().MstBranch.Branch;
+                        int companyId = currentUser.FirstOrDefault().CompanyId;
+                        string company = currentUser.FirstOrDefault().MstCompany.Company;
+
+                        ViewData.Add("BranchId", branchId);
+                        ViewData.Add("Branch", branch);
+                        ViewData.Add("CompanyId", companyId);
+                        ViewData.Add("Company", company);
+
+                        // ==================
+                        // Defaults (Current)
+                        // ==================
+                        int mstUserId = currentUser.FirstOrDefault().Id;
+                        string salesInvoiceName = currentUser.FirstOrDefault().SalesInvoiceName;
+                        int defaultSalesInvoiceDiscountId = currentUser.FirstOrDefault().DefaultSalesInvoiceDiscountId;
+                        string defaultSalesInvoiceDiscount = currentUser.FirstOrDefault().MstDiscount.Discount;
+                        string officialReceiptName = currentUser.FirstOrDefault().OfficialReceiptName;
+                        
+                        ViewData.Add("MstUserId", mstUserId);
+                        ViewData.Add("SalesInvoiceName", salesInvoiceName);
+                        ViewData.Add("defaultSalesInvoiceDiscountId", defaultSalesInvoiceDiscountId);
+                        ViewData.Add("DefaultSalesInvoiceDiscount", defaultSalesInvoiceDiscount);
+                        ViewData.Add("OfficialReceiptName", officialReceiptName);
+
+                        // ================
+                        // System (Current)
+                        // ================
+                        int netIncomeAccountId = currentUser.FirstOrDefault().IncomeAccountId;
+                        string netIncomeAccount = currentUser.FirstOrDefault().MstAccount.Account;
+                        int supplierAdvancesAccountId = currentUser.FirstOrDefault().SupplierAdvancesAccountId;
+                        string supplierAdvancesAccount = currentUser.FirstOrDefault().MstAccount1.Account;
+                        int customerAdvancesAccountId = currentUser.FirstOrDefault().CustomerAdvancesAccountId;
+                        string customerAdvancesAccount = currentUser.FirstOrDefault().MstAccount2.Account;
+                        string inventoryType = currentUser.FirstOrDefault().InventoryType;
+                        bool isIncludeCostStockReports = currentUser.FirstOrDefault().IsIncludeCostStockReports;
+
+                        ViewData.Add("NetIncomeAccountId", netIncomeAccountId);
+                        ViewData.Add("NetIncomeAccount", netIncomeAccount);
+                        ViewData.Add("SupplierAdvancesAccountId", supplierAdvancesAccountId);
+                        ViewData.Add("SupplierAdvancesAccount", supplierAdvancesAccount);
+                        ViewData.Add("CustomerAdvancesAccountId", customerAdvancesAccountId);
+                        ViewData.Add("CustomerAdvancesAccount", customerAdvancesAccount);
+                        ViewData.Add("InventoryType", inventoryType);
+                        ViewData.Add("IsIncludeCostStockReports", isIncludeCostStockReports);
+                    }
                 }
             }
 
