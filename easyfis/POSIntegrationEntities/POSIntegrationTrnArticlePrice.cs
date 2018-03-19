e78608e703662592c7c719d8a3c08f9b1603cc0f
NoahOliverRIgonan <oliverrigonan@gmail.com>
2018-03-19 15:58:51 +0800

    Easyfis POS Integration Item Price

---
+++ easyfis/POSIntegrationEntities/POSIntegrationTrnArticlePrice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace easyfis.POSIntegrationEntities
+{
+    public class POSIntegrationTrnArticlePrice
+    {
+        public String BranchCode { get; set; }
+        public String IPNumber { get; set; }
+        public String IPDate { get; set; }
+        public String Particulars { get; set; }
+        public String ManualIPNumber { get; set; }
+        public Int32 ArticlePriceId { get; set; }
+        public String ItemCode { get; set; }
+        public String ItemDescription { get; set; }
+        public Decimal Price { get; set; }
+        public Decimal TriggerQuantity { get; set; }
+    }
+}
