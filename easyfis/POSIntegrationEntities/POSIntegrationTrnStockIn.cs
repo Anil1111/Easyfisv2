cefce8d65074416e341d34beec397c8e9e188c36
NoahOliverRIgonan <oliverrigonan@gmail.com>
2018-03-21 11:43:00 +0800

    Update POS Integration for Stock In

---
+++ easyfis/POSIntegrationEntities/POSIntegrationTrnStockIn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace easyfis.POSIntegrationEntities
+{
+    public class POSIntegrationTrnStockIn
+    {
+        public String BranchCode { get; set; }
+        public String Branch { get; set; }
+        public String INNumber { get; set; }
+        public String INDate { get; set; }
+        public List<POSIntegrationTrnStockInItem> ListPOSIntegrationTrnStockInItem { get; set; }
+    }
+
+    public class POSIntegrationTrnStockInItem
+    {
+        public Int32 INId { get; set; }
+        public String ItemCode { get; set; }
+        public String Item { get; set; }
+        public String Unit { get; set; }
+        public Decimal Quantity { get; set; }
+        public Decimal Cost { get; set; }
+        public Decimal Amount { get; set; }
+    }
+}
