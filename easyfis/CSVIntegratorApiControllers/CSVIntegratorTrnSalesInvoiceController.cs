84471541fcb3877692da34ad87c7e423b56fd610
NoahOliverRIgonan <oliverrigonan@gmail.com>
2018-04-09 15:10:20 +0800

    API CSV Integrator

---
+++ easyfis/CSVIntegratorApiControllers/CSVIntegratorTrnSalesInvoiceController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using System.Diagnostics;
+
+namespace easyfis.CSVIntegratorApiControllers
+{
+    public class CSVIntegratorTrnSalesInvoiceController : ApiController
+    {
+        // ============
+        // Data Context
+        // ============
+        private Data.easyfisdbDataContext db = new Data.easyfisdbDataContext();
+
+        // ============================
+        // Zero Fill - Document Numbers
+        // ============================
+        public String FillLeadingZeroes(Int32 number, Int32 length)
+        {
+            var result = number.ToString();
+            var pad = length - result.Length;
+            while (pad > 0)
+            {
+                result = '0' + result;
+                pad--;
+            }
+
+            return result;
+        }
+
+        // ==================================
+        // Add Sales Invoice (CSV Integrator)
+        // ==================================
+        [HttpPost, Route("api/add/CSVIntegrator/salesInvoice")]
+        public HttpResponseMessage AddSalesInvoiceCSVIntegrator(CSVIntegratorEntities.CSVIntegratorTrnSalesInvoice objSalesInvoice)
+        {
+            try
+            {
+                Boolean currentBranchExist = false;
+                var currentBranch = from d in db.MstBranches where d.BranchCode.Equals(objSalesInvoice.BranchCode) select d;
+                if (currentBranch.Any())
+                {
+                    currentBranchExist = true;
+                }
+
+                Boolean customersExist = false;
+                var customers = from d in db.MstArticles.OrderBy(d => d.Article) where d.ManualArticleCode.Equals(objSalesInvoice.CustomerManualArticleCode) && d.ArticleTypeId == 2 && d.IsLocked == true select d;
+                if (customers.Any())
+                {
+                    customersExist = true;
+                }
+
+                Boolean termsExist = false;
+                var terms = from d in db.MstTerms.OrderBy(d => d.Term) where d.Term.Equals(objSalesInvoice.Term) && d.IsLocked == true select d;
+                if (terms.Any())
+                {
+                    termsExist = true;
+                }
+
+                Boolean usersExist = false;
+                var users = from d in db.MstUsers.OrderBy(d => d.FullName) where d.IsLocked == true select d;
+                if (users.Any())
+                {
+                    usersExist = true;
+                }
+
+                var defaultSINumber = "0000000001";
+                var lastSalesInvoice = from d in db.TrnSalesInvoices.OrderByDescending(d => d.Id)
+                                       where d.MstBranch.BranchCode.Equals(objSalesInvoice.BranchCode)
+                                       select d;
+
+                if (lastSalesInvoice.Any())
+                {
+                    var SINumber = Convert.ToInt32(lastSalesInvoice.FirstOrDefault().SINumber) + 0000000001;
+                    defaultSINumber = FillLeadingZeroes(SINumber, 10);
+                }
+
+                String errorMessage = "";
+                Boolean isValid = false;
+
+                if (currentBranchExist)
+                {
+                    if (customersExist)
+                    {
+                        if (termsExist)
+                        {
+                            if (usersExist)
+                            {
+                                isValid = true;
+                            }
+                            else
+                            {
+                                errorMessage = "No User!";
+                            }
+                        }
+                        else
+                        {
+                            errorMessage = "No Term!";
+                        }
+                    }
+                    else
+                    {
+                        errorMessage = "No Customer!";
+                    }
+                }
+                else
+                {
+                    errorMessage = "No Branch!";
+                }
+
+                if (isValid)
+                {
+                    Data.TrnSalesInvoice newSalesInvoice = new Data.TrnSalesInvoice
+                    {
+                        BranchId = currentBranch.FirstOrDefault().Id,
+                        SINumber = defaultSINumber,
+                        SIDate = Convert.ToDateTime(objSalesInvoice.SIDate),
+                        DocumentReference = "NA",
+                        CustomerId = customers.FirstOrDefault().Id,
+                        TermId = terms.FirstOrDefault().Id,
+                        Remarks = objSalesInvoice.Remarks,
+                        ManualSINumber = objSalesInvoice.ManualSINumber,
+                        Amount = 0,
+                        PaidAmount = 0,
+                        AdjustmentAmount = 0,
+                        BalanceAmount = 0,
+                        SoldById = users.FirstOrDefault().Id,
+                        PreparedById = users.FirstOrDefault().Id,
+                        CheckedById = users.FirstOrDefault().Id,
+                        ApprovedById = users.FirstOrDefault().Id,
+                        IsLocked = false,
+                        CreatedById = users.FirstOrDefault().Id,
+                        CreatedDateTime = DateTime.Now,
+                        UpdatedById = users.FirstOrDefault().Id,
+                        UpdatedDateTime = DateTime.Now
+                    };
+
+                    db.TrnSalesInvoices.InsertOnSubmit(newSalesInvoice);
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK, newSalesInvoice.SINumber);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, errorMessage);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Something's went wrong from the server.");
+            }
+        }
+    }
+}
---
+++ easyfis/CSVIntegratorApiControllers/CSVIntegratorTrnStockInItemController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Diagnostics;
+
+namespace easyfis.CSVIntegratorApiControllers
+{
+    public class CSVIntegratorTrnStockInItemController : ApiController
+    {
+        // ============
+        // Data Context
+        // ============
+        private Data.easyfisdbDataContext db = new Data.easyfisdbDataContext();
+
+        // ==================================
+        // Add Stock In Item (CSV Integrator)
+        // ==================================
+        [HttpPost, Route("api/add/CSVIntegrator/stockInItem")]
+        public HttpResponseMessage AddStockInItem(List<CSVIntegratorEntities.CSVIntegratorTrnStockInItem> objStockInItems)
+        {
+            try
+            {
+                if (objStockInItems.Any())
+                {
+                    foreach (var objStockInItem in objStockInItems)
+                    {
+                        Boolean stockInExists = false;
+                        var stockIn = from d in db.TrnStockIns where d.MstBranch.BranchCode.Equals(objStockInItem.BranchCode) && d.INNumber.Equals(objStockInItem.INNumber) select d;
+                        if (stockIn.Any())
+                        {
+                            stockInExists = true;
+                        }
+
+                        Boolean itemExists = false;
+                        var item = from d in db.MstArticles where d.ManualArticleCode.Equals(objStockInItem.ItemCode) && d.ArticleTypeId == 1 && d.IsLocked == true select d;
+                        if (item.Any())
+                        {
+                            itemExists = true;
+                        }
+
+                        Boolean isValid = false;
+                        if (stockInExists)
+                        {
+                            if (itemExists)
+                            {
+                                if (!stockIn.FirstOrDefault().IsLocked)
+                                {
+                                    isValid = true;
+                                }
+                            }
+                        }
+
+                        if (isValid)
+                        {
+                            var conversionUnit = from d in db.MstArticleUnits
+                                                 where d.ArticleId == item.FirstOrDefault().Id
+                                                 && d.UnitId == item.FirstOrDefault().UnitId
+                                                 && d.MstArticle.IsLocked == true
+                                                 select d;
+
+                            if (conversionUnit.Any())
+                            {
+                                Decimal baseQuantity = objStockInItem.Quantity * 1;
+                                if (conversionUnit.FirstOrDefault().Multiplier > 0)
+                                {
+                                    baseQuantity = objStockInItem.Quantity * (1 / conversionUnit.FirstOrDefault().Multiplier);
+                                }
+
+                                Decimal baseCost = objStockInItem.Amount;
+                                if (baseQuantity > 0)
+                                {
+                                    baseCost = objStockInItem.Amount / baseQuantity;
+                                }
+
+                                Data.TrnStockInItem newStockInItem = new Data.TrnStockInItem
+                                {
+                                    INId = stockIn.FirstOrDefault().Id,
+                                    ItemId = item.FirstOrDefault().Id,
+                                    Particulars = objStockInItem.Particulars,
+                                    UnitId = item.FirstOrDefault().UnitId,
+                                    Quantity = objStockInItem.Quantity,
+                                    Cost = objStockInItem.Cost,
+                                    Amount = objStockInItem.Amount,
+                                    BaseUnitId = item.FirstOrDefault().UnitId,
+                                    BaseQuantity = baseQuantity,
+                                    BaseCost = baseCost
+                                };
+
+                                db.TrnStockInItems.InsertOnSubmit(newStockInItem);
+                            }
+                        }
+                    }
+
+                    db.SubmitChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Empty!");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Something's went wrong from the server.");
+            }
+        }
+    }
+}
