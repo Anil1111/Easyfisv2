7497dfb6716b2b88317edddd26339d84165f655b
NoahOliverRIgonan <oliverrigonan@gmail.com>
2018-08-11 16:50:57 +0800

    Added new API and Publish to other Server

---
+++ easyfis/Integration/InnosoftPOS/ApiControllers/ApiInnosoftPOSIntegrationMstDiscountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace easyfis.Integration.InnosoftPOS.ApiControllers
+{
+    public class ApiInnosoftPOSIntegrationMstDiscountController : ApiController
+    {
+        // ============
+        // Data Context
+        // ============
+        private Data.easyfisdbDataContext db = new Data.easyfisdbDataContext();
+
+        // ========================================
+        // List Discount (Innosoft POS Integration)
+        // ========================================
+        [HttpGet, Route("api/innosoftPOSIntegration/discount/list")]
+        public List<Entities.InnosoftPOSIntegrationMstDiscount> ListInnosoftPOSIntegrationDiscount()
+        {
+            var discounts = from d in db.MstDiscounts.OrderBy(d => d.Discount)
+                            select new Entities.InnosoftPOSIntegrationMstDiscount
+                            {
+                                Id = d.Id,
+                                Discount = d.Discount
+                            };
+
+            return discounts.ToList();
+        }
+    }
+}
---
+++ easyfis/Integration/InnosoftPOS/Entities/InnosoftPOSIntegrationMstUnit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace easyfis.Integration.InnosoftPOS.Entities
+{
+    public class InnosoftPOSIntegrationMstUnit
+    {
+        public Int32 Id { get; set; }
+        public String Unit { get; set; }
+    }
+}
