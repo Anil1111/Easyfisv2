d565bdcc79534653da6538749a690de11d11b38d
NoahOliverRIgonan <oliverrigonan@gmail.com>
2018-09-06 15:03:51 +0800

    Added Template PDF

--- easyfis/Reports/RepBIRCASGeneralLedgerController.cs
+++ easyfis/Reports/RepBIRCASGeneralLedgerController.cs
@@ -1,17 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace easyfis.Reports
 {
     public class RepBIRCASGeneralLedgerController : Controller
     {
-        // GET: RepBIRCASGeneralLedger
-        public ActionResult Index()
+        // ============
+        // Data Context
+        // ============
+        private Data.easyfisdbDataContext db = new Data.easyfisdbDataContext();
+
+        // =====================
+        // Preview and Print PDF
+        // =====================
+        [Authorize]
+        public ActionResult BIRCASGeneralLedger(String StartDate, String EndDate, String CompanyId, String BranchId)
         {
-            return View();
+            // ==============================
+            // PDF Settings and Customization
+            // ==============================
+            MemoryStream workStream = new MemoryStream();
+            Rectangle rectangle = new Rectangle(PageSize.A3);
+            Document document = new Document(rectangle, 72, 72, 72, 72);
+            document.SetMargins(30f, 30f, 30f, 30f);
+            PdfWriter.GetInstance(document, workStream).CloseStream = false;
+
+            // =============
+            // Document Open
+            // =============
+            document.Open();
+
+            // ============
+            // Fonts Styles
+            // ============
+            Font fontArial17Bold = FontFactory.GetFont("Arial", 17, Font.BOLD);
+            Font fontArial12Bold = FontFactory.GetFont("Arial", 12, Font.BOLD);
+            Font fontArial12 = FontFactory.GetFont("Arial", 12);
+            Font fontArial11Bold = FontFactory.GetFont("Arial", 11, Font.BOLD);
+            Font fontArial11 = FontFactory.GetFont("Arial", 11);
+            Font fontArial10Bold = FontFactory.GetFont("Arial", 10, Font.BOLD);
+            Font fontArial10 = FontFactory.GetFont("Arial", 10);
+
+            // ====
+            // Line 
+            // ====
+            Paragraph line = new Paragraph(new Chunk(new iTextSharp.text.pdf.draw.LineSeparator(0.0F, 100.0F, BaseColor.BLACK, Element.ALIGN_LEFT, 4.5F)));
+
+            // ==============
+            // Company Detail
+            // ==============
+            var company = from d in db.MstCompanies where d.Id == Convert.ToInt32(CompanyId) select d;
+            var branch = from d in db.MstBranches where d.Id == Convert.ToInt32(BranchId) select d;
+
+            // ===========
+            // Header Page
+            // ===========
+            PdfPTable header = new PdfPTable(2);
+            header.SetWidths(new float[] { 100f, 75f });
+            header.WidthPercentage = 100;
+            header.AddCell(new PdfPCell(new Phrase(company.FirstOrDefault().Company, fontArial17Bold)) { Border = 0 });
+            header.AddCell(new PdfPCell(new Phrase("General Ledger", fontArial17Bold)) { Border = 0, HorizontalAlignment = 2 });
+            header.AddCell(new PdfPCell(new Phrase(company.FirstOrDefault().Address, fontArial11)) { Border = 0, PaddingTop = 5f });
+            header.AddCell(new PdfPCell(new Phrase(branch.FirstOrDefault().Branch, fontArial11)) { Border = 0, PaddingTop = 5f, HorizontalAlignment = 2 });
+            header.AddCell(new PdfPCell(new Phrase(company.FirstOrDefault().ContactNumber, fontArial11)) { Border = 0, PaddingTop = 5f });
+            header.AddCell(new PdfPCell(new Phrase("Date Printed: " + DateTime.Now.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture) + " " + DateTime.Now.ToString("hh:mm:ss tt"), fontArial11)) { Border = 0, PaddingTop = 5f, HorizontalAlignment = 2 });
+            header.AddCell(new PdfPCell(new Phrase("TIN: " + company.FirstOrDefault().TaxNumber, fontArial11)) { Border = 0, PaddingTop = 5f });
+            header.AddCell(new PdfPCell(new Phrase("", fontArial11)) { Border = 0 });
+            document.Add(header);
+            document.Add(line);
+
+            // ==================
+            // Date Range Filters
+            // ==================
+            PdfPTable dateRangeFilters = new PdfPTable(1);
+            dateRangeFilters.SetWidths(new float[] { 100f });
+            dateRangeFilters.WidthPercentage = 100;
+            dateRangeFilters.AddCell(new PdfPCell(new Phrase("Date Start:  " + Convert.ToDateTime(StartDate).ToString("MM/dd/yyyy", CultureInfo.InvariantCulture), fontArial11)) { Border = 0, HorizontalAlignment = 0, PaddingTop = 8f });
+            dateRangeFilters.AddCell(new PdfPCell(new Phrase("Date End:   " + Convert.ToDateTime(EndDate).ToString("MM/dd/yyyy", CultureInfo.InvariantCulture), fontArial11)) { Border = 0, HorizontalAlignment = 0, PaddingTop = 5f });
+            document.Add(dateRangeFilters);
+
+            // ==============
+            // Document Close
+            // ==============
+            document.Close();
+
+            // ================
+            // Byte File Stream
+            // ================
+            byte[] byteInfo = workStream.ToArray();
+            workStream.Write(byteInfo, 0, byteInfo.Length);
+            workStream.Position = 0;
+
+            return new FileStreamResult(workStream, "application/pdf");
         }
     }
 }
